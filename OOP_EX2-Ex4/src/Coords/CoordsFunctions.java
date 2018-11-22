5438882717fd5b4590163b0a1924ffd5539cf2e4
Simple Man <Simple Man@Lmobile>
2018-11-22 12:45:24 +0200

    Fixed Calculations.

--- OOP_EX2-Ex4/src/Coords/CoordsFunctions.java
+++ OOP_EX2-Ex4/src/Coords/CoordsFunctions.java
@@ -8,19 +8,26 @@ import Geom.Point3D;
 public class CoordsFunctions implements coords_converter {
 	/** computes a new point which is the gps point transformed by a 3D vector (in meters)*/
 	public Point3D add(Point3D gps, Point3D local_vector_in_meter) {//לללכת מספר מסוים של צעדים צפונה או דרומה ומספר צעדים מסויים מזרחה או מערבה ולהחזיר את המיקום החדש בנקודת ג'יפיאס
-		Point3D p=new Point3D (D2M(gps,gps.x()),D2M(gps,gps.y()),gps.z());
-		double x=M2D(gps, p.x()+local_vector_in_meter.x());
-		double y=M2D(gps, p.y()+local_vector_in_meter.y());
+		Point3D p=new Point3D (0,0,0);
+		try {
+			p = new Point3D (D2M(gps));
+		} catch (Exception e) {
+			System.out.println("worng output");
+			e.printStackTrace();
+		}
+		double x=p.x()+local_vector_in_meter.x();
+		double y=p.y()+local_vector_in_meter.y();
 		double z=p.z()+local_vector_in_meter.z();
 		Point3D p1=new Point3D (x,y,z);
+		
 		return p1;
 	}
 	
 	
 	/** computes the 3D distance (in meters) between the two gps like points */
 	public double distance3d(Point3D gps0, Point3D gps1) {
-		double x=D2M(gps0,gps1.x()-gps0.x());
-		double y=(D2M(gps0,gps1.y()-gps0.y()))*(Math.cos((gps0.x()*Math.PI)/180));
+		double x=Math.sin(Point3D.d2r(gps1.x()-gps0.x()))*EarthR;
+		double y=Math.sin(Point3D.d2r(gps1.x()-gps0.x()))*EarthR*(Math.cos((gps0.x()*Math.PI)/180));
 		double distance=Math.sqrt((x*x)+(y*y));
 		return distance;
 	}
@@ -60,27 +67,42 @@ public class CoordsFunctions implements coords_converter {
 	 * @param y
 	 * @return
 	 */
-	public	double D2M(Point3D gps,double a) {
-		double x=(Math.sin(gps.d2r(a)))*EarthP;
-		return x;
-	}
-	public double M2D(Point3D gpsM,double a){//יכול להיות שהלן לום לא קשור צריך לבדוק
-		double x=Math.asin(a/EarthP);
-		gpsM.r2d(x);
-		return x;
-	}
-	public	Point3D D2M(Point3D gps) {
-		double x=D2M(gps,gps.x());
-		double y=D2M(gps,gps.y());
-		Point3D p=new Point3D(x,y,gps.z());
+	
+	public	Point3D D2M(Point3D gps)throws Exception {
+		if(isValid_GPS_Point(gps)==false) {
+			throw new Exception ("gps point incorrect!");
+		}
+		double x=Math.sin(Point3D.d2r(gps.x()))*Math.cos(Point3D.d2r(gps.y()))*EarthR;
+		double y=Math.sin(Point3D.d2r(gps.x()))*Math.sin(Point3D.d2r(gps.y()))*EarthR;
+		double z=Math.cos(Point3D.d2r(gps.x()))*EarthR;
+		Point3D p=new Point3D(x,y,z);
 		return p;
 	}
-	public	Point3D M2D(Point3D gps) {
-		double x=M2D(gps,gps.x());
-		double y=M2D(gps,gps.y());
-		Point3D p=new Point3D(x,y,gps.z());
+	public	Point3D M2D(Point3D gps) throws Exception {
+		double x=Math.sqrt((gps.x()*gps.x())+(gps.y()*gps.y()));
+		x=Point3D.r2d(x);
+		
+		double y;
+		if(gps.x()>0) {
+			y=Point3D.r2d(Math.atan(gps.y()/gps.x()));
+		}
+		else{
+			y=Point3D.r2d(Math.atan(gps.y()/gps.x())+Math.PI);
+		}
+		
+		double z=Math.asin(gps.x()/EarthR);
+		z=Point3D.r2d(z);
+		
+		Point3D p=new Point3D(x,y,z);
+		if(isValid_GPS_Point(p)==false) {
+			throw new Exception ("gps point incorrect!");
+		}
+		
 		return p;
+		
+		
 	}
+	
 	private List GpsList=new ArrayList();
-	private final int EarthP=6371000;
+	private final int EarthR=6371000;
 }
--- OOP_EX2-Ex4/src/Coords/pointest.java
+++ OOP_EX2-Ex4/src/Coords/pointest.java
@@ -4,10 +4,19 @@ import Geom.Point3D;
 
 public class pointest {
 public static void main (String[]args) {
-	Point3D p=new Point3D(32.103315,35.209039);
-	Point3D p1=new Point3D(32.106352,35.205225);
-	CoordsFunctions c=new CoordsFunctions();
-	System.out.println(c.distance3d(p, p1));
+//	Point3D p=new Point3D(32.103315,35.209039);
+//	Point3D p1=new Point3D(32.106352,35.205225);
+//	CoordsFunctions c=new CoordsFunctions();
+//	System.out.println(c.distance3d(p, p1));
+	try {
+		int p []=new int [2];
+		p [2]=11;
+	} catch (Exception e) {
+		System.out.println("worng output");
+		e.printStackTrace();
+	}
+	System.out.println("kelev");
+	
 	
 }
 }
