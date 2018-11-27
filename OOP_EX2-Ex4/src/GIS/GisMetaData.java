51ac74ad53e4a06fb4aa6938ee47c0bf778524e0
Simple Man <Simple Man@Lmobile>
2018-11-27 18:45:22 +0200

    New classes "GisLayer" "GisMetaData" "GisProject".

---
+++ OOP_EX2-Ex4/src/GIS/GisMetaData.java
@@ -0,0 +1,19 @@
+package GIS;
+
+import Geom.Point3D;
+
+public class GisMetaData implements Meta_data {
+
+	@Override
+	public long getUTC() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Point3D get_Orientation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
