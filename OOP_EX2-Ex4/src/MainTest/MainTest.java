857fe0fdda4846d96d499c794be36c7b046675a6
michael_lemberger <michael_lemberger_mail@gmail.com>
2018-12-02 16:11:23 +0200

    done!

--- OOP_EX2-Ex4/src/MainTest/MainTest.java
+++ OOP_EX2-Ex4/src/MainTest/MainTest.java
@@ -8,24 +8,24 @@ public class MainTest {
 	public static void main(String[] args) throws Exception {
 		
 		//enter a adress to create a kml project from the directories csv files.
-		String adress1 = "C:\\Users\\liron\\Downloads\\Ex2";
+		String adress1 = "C:\\Users\\Simple Man\\Desktop\\ex2";
 		
 		//create MultiCsv object
 		MultiCsv m = new MultiCsv(adress1);
 		
 		//create the kml file from the MultiCsv, save it on this path.
-		m.getProject().ProjectToKml("C:\\kmlProject.kml");
+		m.getProject().ProjectToKml("C:\\Users\\Simple Man\\Desktop\\ex2\\kmlProject.kml");
 		
 		/**************************************************************************************/
 		
 		//enter a adress to create a kml layer from the directories csv file.
-		String adress2 ="C:\\Users\\liron\\Downloads\\Ex2-1\\Ex2\\data\\WigleWifi_20171201110209.csv";
+		String adress2 ="C:\\Users\\Simple Man\\Desktop\\ex2\\WigleWifi_20171201110209.csv";
 		
 		//create GisLayer object.
 		GisLayer layer = new GisLayer(adress2);
 		
 		//create the kml file from the GisLayer, save it on this path.
-		layer.LeyerToKML("C:\\kmlLayer.kml");
+		layer.LeyerToKML("C:\\Users\\Simple Man\\Desktop\\ex2\\kmlProject.kml");
 		
 		
 
