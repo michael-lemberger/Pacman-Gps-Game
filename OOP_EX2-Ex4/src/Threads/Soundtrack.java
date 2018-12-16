bfc6803aa96a348900e9255a579ce9babfb512e0
Simple Man <Simple Man@Lmobile>
2018-12-16 17:10:19 +0200

    Soundtrack added

--- OOP_EX2-Ex4/src/Threads/Soundtrack.java
+++ OOP_EX2-Ex4/src/Threads/Soundtrack.java
@@ -1,5 +1,18 @@
 package Threads;
 
+import File_format.SimplePlayer;
+
 public class Soundtrack {
+	static String path = "res/Pac-man theme remix - By Arsenic1987.mp3";
 	
+	public static void main(String[] args) {
+		SimplePlayer player = new SimplePlayer(path);
+		Thread t = new Thread(player);
+		t.start();
+		
+		
+		for (int i = 0; i < 1000000; i++) {
+			System.out.println(i);
+		}
+	}
 }
