a912d36bcb89c169b494b7aaec7caceb9311d977
michael_lemberger <michael_lemberger_mail@gmail.com>
2018-12-17 11:18:39 +0200

    Added resource's

--- OOP_EX2-Ex4/src/MainTest/MainTest.java
+++ OOP_EX2-Ex4/src/MainTest/MainTest.java
@@ -12,7 +12,7 @@ public class MainTest {
 
 	public static void main(String[] args) throws Exception {
 		
-		Game game=new Game ("C:\\Users\\Simple Man\\Desktop\\מדעי המחשב\\Ex3 (2) (2)\\Ex3\\Ex3_data\\data\\game_1543684662657.csv");
+		Game game=new Game ("res\\game_1543684662657.csv");
 		ShortestPathAlgo s=new ShortestPathAlgo(game);
 		Iterator<Pacman>pacmans=game.pacmans.iterator();
 		int i=1;
